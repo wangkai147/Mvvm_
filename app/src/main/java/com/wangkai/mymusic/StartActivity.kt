bb7988387456e803a0784b5bd6a0db36c17ad7b4
wangkai <2925285800@outlook.com>
2021-04-17 00:26:32 +0800

    添加启动页

---
+++ app/src/main/java/com/wangkai/mymusic/StartActivity.kt
@@ -0,0 +1,56 @@
+package com.wangkai.mymusic
+
+/**
+ * @Description: 启动页
+ * @Author: JWangZzz
+ * @CreateDate: 2021.4.16
+ */
+
+import android.os.Bundle
+import com.wangkai.base.activity.ViewBindingBaseActivity
+import com.wangkai.mymusic.databinding.ActivityStartBinding
+import com.wangkai.base.manager.startActivity
+
+class StartActivity : ViewBindingBaseActivity<ActivityStartBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mViewBinding.animationView.playAnimation()
+        setFullScreen()
+
+    }
+
+    override fun setCreateBefore() {
+    }
+
+    override fun setContentBefore() {
+    }
+
+    override fun getBundleExtras(extras: Bundle?) {
+
+    }
+
+    override fun initData() {
+        //开屏动画
+        val list = mutableListOf(
+            "pride-hard-seltzer.json",
+            "lamsa-splash-screen.json",
+            "ramadan-kareem-hello-doc.json",
+            "logo-animation.json"
+        )
+        mViewBinding.animationView.addAnimatorUpdateListener {
+            if(it.animatedFraction == 1f) {
+                startActivity<MainActivityViewBinding>()
+            }
+        }
+        mViewBinding.animationView.setAnimation(list.random())
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initViewModel() {
+
+    }
+}
